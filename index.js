8c299c5e90d7cad7fa1cbde08c57d2e606f0fae6
Ndickers <bryondickers@gmail.com>
2024-05-25 04:17:29 +0300

    Update styling

--- index.js
+++ index.js
@@ -6,11 +6,24 @@ document.addEventListener("DOMContentLoaded", async function () {
   const selectInput = document.querySelector("#region");
   const search = document.querySelector("#search");
 
-  // search.addEventListener("input", async function ({ target }) {
-  //   section.innerHTML = "";
-  //   const region = await filterCountries(target.value);
-  //   await createCountries(region, section);
-  // });
+  // implement dark mode
+  const changeMode = document.querySelector(".change-mode-btn");
+  changeMode.addEventListener("click", function () {
+    const getRoot = document.querySelector(":root");
+    const themeIcon = document.querySelector(".mode-icon");
+    const backIcon = document.querySelector(".back-icon");
+    if (getRoot.classList.contains("dark")) {
+      getRoot.classList.remove("dark");
+      themeIcon.setAttribute("src", "./assets/light-icon.png");
+      backIcon.setAttribute("src", "./assets/back-arrow.png");
+    } else {
+      getRoot.classList.add("dark");
+      themeIcon.setAttribute("src", "./assets/dark-icon.png");
+      backIcon.setAttribute("src", "./assets/back-arrow-light.png");
+    }
+
+    console.log(getRoot);
+  });
 
   selectInput.addEventListener("change", async function ({ target }) {
     section.innerHTML = "";
@@ -58,36 +71,46 @@ function createCountries(countryArray, section) {
 }
 
 function detailPage(country) {
-  return `<a href="./index.html">
-  <img class="back-icon" src="./assets/back-arrow.svg" alt="" srcset="" />
+  console.log(country.languages);
+  return `<div class="detail-container">
+  <a href="./index.html">
+  <img class="back-icon" src="./assets/back-arrow.png" alt="" srcset="" />
   back</a>
 
-  <div>
-    <img src=${country.flags.svg} alt="" srcset="" />
- <div>
- <div>
- <h2>${country.name}</h2>
- <p>Native Name: <span>Belgie</span></p>
- <p>Population:<span>${country.population}</span></p>
- <p>Region: <span>${country.region}</span></p>
- <p>Sub Region: <span>${country.subregion}</span></p>
- <p>Capital: <span>${country.capital}</span></p>
-</div>
-<div>
- <p>Top Level Domain: <span>${country.topLevelDomain}</span></p>
- <p>Currencies:<span>${country.currencies[0].code}</span></p>
- <p>Languages: <span>${
-   (country.languages[0].name, country.languages[0].native)
- }</span></p>
-</div>
- </div>
+  <div class="detail-flex">
+    <img class="country-img" src=${country.flags.svg} alt="" srcset="" />
+<div class="country-details">
+<div class="split-details">
+<h2>${country.name}</h2>
+<div class="split-content">
+<div class="detail-page">
 
-    <h3>Border Countries:</h3>
-    <div>
-      <p>${country?.borders[0]}</p>
-      <p>${country?.borders[1]}</p>
-      <p>${country?.borders[2]}</p>
-    </div>
+<p>Native Name: <span>Belgie</span></p>
+<p>Population:<span>${country.population}</span></p>
+<p>Region: <span>${country.region}</span></p>
+<p>Sub Region: <span>${country.subregion}</span></p>
+<p>Capital: <span>${country.capital}</span></p>
+</div>
+<div class="domain-sec">
+<p>Top Level Domain: <span>${country.topLevelDomain}</span></p>
+<p>Currencies:<span>${country.currencies[0].code}</span></p>
+<p>Languages: <span>${
+    (country.languages[0].name, country.languages[0].nativeName)
+  }</span></p>
+</div></div>
+</div>
+
+
+   <div class="flex-border">
+   <h3>Border Countries:</h3>
+   <div class="borders">
+     <p>${country.borders.length !== 0 ? country.borders[0] : "No border"}</p>
+     <p>${country.borders.length !== 1 ? country.borders[1] : "No border"}</p>
+     <p>${country.borders.length !== 2 ? country.borders[2] : "No border"}</p>
+   </div>
+   </div>
+</div>
+  </div>
   </div>
 `;
 }
